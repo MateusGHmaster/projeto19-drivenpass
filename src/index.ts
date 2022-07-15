60adaa221adf73d14f6adbd73bed7c05a7f5f030
Mateus <mateusoliveira469@gmail.com>
2022-07-15 11:19:44 -0400

    feat: initial routers structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+import express, { json } from 'express';
+import './config/setup.js';
+import router from './routers/index.js';
 import 'express-async-errors';
+import cors from 'cors';
+
+const app = express();
+
+app.use(json());
+app.use(router);
+/*app.use(handleErrorsMiddleware); */
+
+const PORT = +process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+    console.log(`Working and running on port ${PORT}!`);
+
+});
