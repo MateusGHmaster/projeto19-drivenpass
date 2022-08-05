c37fbbeb09fa6382677b29cc97fad2fcd0e03633
Rodrigodcs <rodrigodcsee@gmail.com>
2022-08-05 11:03:39 -0300

    Preparando revisão de código

--- src/schemas/noteSchema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-import { CreateNoteData } from '../repositories/noteRepository.js';
-
-export const noteSchema = Joi.object({
-
-    title: Joi.string().max(50).required(),
-    note: Joi.string().max(1000).required()
-
-});
